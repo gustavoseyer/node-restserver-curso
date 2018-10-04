4e73b644b580ae06429b1f88bb8720c33c8bad5e
Gustavo <gustavo_seyer@hotmail.com>
2018-10-04 10:12:04 -0500

    RestServer-Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,22 @@
 // Puerto
 //==============
 
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
+
+// ==============
+// Entorno
+//==============
+process.env.Node_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============
+// Base de datos
+//==============
+
+let urlDB;
+if(process.env.Node_ENV){
+    urlDB= 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB='mongodb://cafe-user:polopolo00@ds123003.mlab.com:23003/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,46 +1,23 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const colors =require('colors');
+
 const bodyParser = require('body-parser')
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-})
+app.use( require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-
-    let body = req.body
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-
-    res.json({
-        persona: body
-    })
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-})
-
+mongoose.connect(process.env.URLDB,(err,res) =>{
+    if (err) throw err;
+    console.log('Base de datos online'.green);
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando un Puerto: ', 3000);
