bb300e9510f3e4daeeccf217ba72c71090a53cb3
Gustavo <gustavo_seyer@hotmail.com>
2018-10-04 10:53:05 -0500

    RestServer-Usuario-3

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ process.env.Node_ENV = process.env.NODE_ENV || 'dev';
 //==============
 
 let urlDB;
-if(process.env.Node_ENV){
+if(process.env.Node_ENV === 'dev'){
     urlDB= 'mongodb://localhost:27017/cafe'
 } else {
     urlDB='mongodb://cafe-user:polopolo00@ds123003.mlab.com:23003/cafe';
