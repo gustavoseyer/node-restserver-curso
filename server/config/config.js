42708e39bed8cc96011359ef56bc7add90030c5c
Gustavo <gustavo_seyer@hotmail.com>
2018-10-04 10:54:28 -0500

    RestServer-Usuario-4

--- server/config/config.js
+++ server/config/config.js
@@ -7,14 +7,14 @@ process.env.PORT = process.env.PORT || 3000;
 // ==============
 // Entorno
 //==============
-process.env.Node_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ==============
 // Base de datos
 //==============
 
 let urlDB;
-if(process.env.Node_ENV === 'dev'){
+if(process.env.NODE_ENV === 'dev'){
     urlDB= 'mongodb://localhost:27017/cafe'
 } else {
     urlDB='mongodb://cafe-user:polopolo00@ds123003.mlab.com:23003/cafe';
